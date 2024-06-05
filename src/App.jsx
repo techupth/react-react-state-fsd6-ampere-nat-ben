da15e537be28567636dd4d71bcb8aedb85739982
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-06-05 11:22:22 +0700

    finished assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("Greeting Message");
+
+  const handleClick = (e) => {
+    setMessage(e.target.value);
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button value="สวัสดี" onClick={handleClick}>
+          สวัสดี!
+        </button>
+        <button value="Hi!" onClick={handleClick}>
+          Hi!
+        </button>
+        <button value="你好!" onClick={handleClick}>
+          你好!
+        </button>
       </div>
     </div>
   );
